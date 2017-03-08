83ab96a62a1ac6cd74e95e9ded53b98c3c9d9090
abedbanna <abedbanna2008@gmail.com>
2017-03-08 09:16:01 +0200

    Add files via upload

---
+++ Auth/login.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: abanna
+ * Date: 3/8/2017
+ * Time: 8:23 AM
+ */
+echo "User name is:".$_GET['txtUserName']."<br>";
+echo "Password is:".$_GET['txtPassword'];
